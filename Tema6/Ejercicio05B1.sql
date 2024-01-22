96860de5d4b1bf35406ff03aae52aee7ae7fde99
Patricia Alonso Ayllón <alonso.aypat22@triana.salesianos.edu>
2024-01-22 10:03:21 +0100

    lgshdk

--- Tema6/Ejercicio05B1.sql
+++ Tema6/Ejercicio05B1.sql
@@ -4,18 +4,18 @@ CREATE TABLE alumnos(
 	direccion_alumno VARCHAR(250) NOT NULL,
 	telefono_alumno VARCHAR(14) NOT NULL,
 	edad_alumno INTEGER,
-	empresa VARCHAR(300),
+	empresa VARCHAR(10) NOT NULL,
 	CONSTRAINT pk_alumnos PRIMARY KEY (dni_alumno)
 );
 CREATE TABLE empresas(
-	cif_empresa VARCHAR(12),
-	nombre_empresa VARCHAR(250) NOT NULL,
+	cif_empresa VARCHAR(10),
+	nombre_empresa VARCHAR(300) NOT NULL,
 	direccion_empresa VARCHAR(250),
 	telefono_empresa VARCHAR(14) NOT NULL,
 	CONSTRAINT pk_empresas PRIMARY KEY (cif_empresa)
 );
 CREATE TABLE cursos(
-	n_concreto INTEGER,
+	n_concreto INTEGER, --SERIAL
 	fecha_inicio DATE NOT NULL,
 	fecha_fin DATE NOT NULL,
 	dni_profesor VARCHAR(12) NOT NULL,
@@ -44,7 +44,7 @@ CREATE TABLE tipos_cursos(
 );
 
 ALTER TABLE alumnos ADD CONSTRAINT fk_alumnos_empresa 
-FOREIGN KEY (empresa) REFERENCES empresas;
+FOREIGN KEY (empresa) REFERENCES empresas; --REFERENCES empresas(cif_empresa)
 
 ALTER TABLE alumnos_asisten ADD CONSTRAINT fk_alumnos_asisten_dni
 FOREIGN KEY (dni_alumno) REFERENCES alumnos;
@@ -62,13 +62,14 @@ INSERT INTO empresas(nombre_empresa, cif_empresa, direccion_empresa, telefono_em
 VALUES ('HP', '567098345', 'Calle Silvio', '67987534'),
 		('Mac', '789098456', 'Calle Betis', '89756786'),
 		('Lenovo', '789078456', 'Calle Betis', '96456786');
-INSERT INTO alumnos(nombre_alumno, dni_alumno, direccion_alumno, telefono_alumno, edad_alumno, empresa)
-VALUES ('Jose Luis Martín', '67854321R', 'Calle Virgen de Fatima', '9546578932', 18, 'HP'),
-		('Maria Luisa', '67674321R', 'Calle Rockero', '9646578932', 20, 'HP'),
-		('Jorge Juan', '678345123R', 'Calle Reyes', '9546578932', 17, 'HP'),
-		('Jose Luis Martín', '5854321R', 'Calle Virgen de Fatima', '9546578932', 18, 'Mac'),
-		('Maria Luisa', '67664321R', 'Calle Rockero', '9646578932', 20, 'Mac'),
-		('Jorge Juan', '678745123R', 'Calle Reyes', '9546578932', 17, 'Mac'),
-		('Jose Luis Martín', '67854821R', 'Calle Virgen de Fatima', '9546578932', 18, 'Lenovo'),
-		('Maria Luisa', '69674321R', 'Calle Rockero', '9646578932', 20, 'Lenovo'),
-		('Jorge Juan', '678045123R', 'Calle Reyes', '9546578932', 17, 'Lenovo');
+INSERT INTO alumnos(nombre_alumno, dni_alumno, direccion_alumno, telefono_alumno, edad_alumno, empresa)-- EN EMPRESA HAY Q ESCRIBIR EL CIF
+VALUES ('Jose Luis Martín', '67854321R', 'Calle Virgen de Fatima', '9546578932', 18, '567098345'),
+		('Maria Luisa', '67674321R', 'Calle Rockero', '9646578932', 20, '567098345'),
+		('Jorge Juan', '678345123R', 'Calle Reyes', '9546578932', 17, '567098345'),
+		('Jose Luis Martín', '5854321R', 'Calle Virgen de Fatima', '9546578932', 18, '789098456'),
+		('Maria Luisa', '67664321R', 'Calle Rockero', '9646578932', 20, '789098456'),
+		('Jorge Juan', '678745123R', 'Calle Reyes', '9546578932', 17, '789098456'),
+		('Jose Luis Martín', '67854821R', 'Calle Virgen de Fatima', '9546578932', 18, '789078456'),
+		('Maria Luisa', '69674321R', 'Calle Rockero', '9646578932', 20, '789078456'),
+		('Jorge Juan', '678045123R', 'Calle Reyes', '9546578932', 17, '789078456');
+INSERT INTO tipo_cursos(titulo, programa, duracion)
